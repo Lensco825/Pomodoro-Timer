aacb9b5e54075de1dbc2c901dadbfbc9efb8ceab
Lens <lensdorsainvil1@gmail.com>
2024-03-20 23:52:56 -0400

    Added script file and seperated timer innerHTML into an array

---
+++ script.js
@@ -0,0 +1,6 @@
+var timer = document.querySelector('.timer');
+var button = document.querySelector('.timerButton');
+
+let innerTimerContent = Array.from(timer.innerHTML);
+
+console.log(innerTimerContent);
