50b5bfa91f8b473a7437af50bfe73d44b77dc0c6
Lens <lensdorsainvil1@gmail.com>
2024-03-30 17:08:52 -0400

    converted innerHTML to javascript numbers

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 var timer = document.querySelector('.timer');
 var button = document.querySelector('.timerButton');
+var minutes = document.getElementById('minutes').innerHTML;
+var seconds = document.getElementById('seconds').innerHTML;
 
-let innerTimerContent = Array.from(timer.innerHTML);
 
-console.log(innerTimerContent);
+console.log(minutes);
+console.log(seconds);
+
+
+function timeTicking() {
+    
+    if (x == 0) {
+        x = 59;
+    }
+    else {
+        return x - 1;
+    }
+}
+
+setTimeout(timeTicking, 1000);
