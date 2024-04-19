1db0cb298de21a6018e9f042e6f5b13cd5034776
Lens <lensdorsainvil1@gmail.com>
2024-04-19 14:23:09 -0400

    Completed timeDown function code, the timer seems to now fully work well

--- script.js
+++ script.js
@@ -31,7 +31,19 @@ function timeDown() {
         secondsTens.innerHTML = parseInt(secondsTens.innerHTML) + 5;
         secondsOnes.innerHTML = parseInt(secondsOnes.innerHTML) + 9;
         minutesOnes.innerHTML = parseInt(minutesOnes.innerHTML) - 1;
+        console.log("seconds reset");
        }
+
+       if (parseInt(secondsOnes.innerHTML) === 0) {
+        secondsTens.innerHTML = parseInt(secondsTens.innerHTML) - 1;
+        secondsOnes.innerHTML = parseInt(secondsOnes.innerHTML) + 10;
+        console.log('tenths of a second have lowered');
+       }
+
+       if (parseInt(secondsOnes.innerHTML) === 0 && parseInt(secondsTens.innerHTML) === 0 && parseInt(minutesOnes.innerHTML) === 0) {
+        minutesTens.innerHTML = parseInt(minutesTens.innerHTML) - 1;    
+       }
+
        else {
         secondsOnes.innerHTML = parseInt(secondsOnes.innerHTML) - 1;
        }
