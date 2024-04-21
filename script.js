42538fdeac3525525673970f6f11c5bce3ca6497
Lens <lensdorsainvil1@gmail.com>
2024-04-21 14:28:48 -0400

    fixed timeDown function by stopping the minutesOnes element from going below 0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var timer = document.querySelector('.timer');
 var button = document.querySelector('.timerButton');
-var minutesTens = document.getElementById('minuteTens');
+var minutesTens = document.getElementById('minutesTens');
 var minutesOnes = document.getElementById('minutesOnes');
 var secondsTens = document.getElementById('secondsTens');
 let secondsOnes = document.getElementById('secondsOnes');
@@ -27,6 +27,13 @@ button.addEventListener('click', () => {
 
 function timeDown() {
     if (isTimeRunning) {
+
+        if (parseInt(secondsOnes.innerHTML) === 0 && parseInt(secondsTens.innerHTML) === 0 && parseInt(minutesOnes.innerHTML) === 0) {
+         minutesTens.innerHTML = parseInt(minutesTens.innerHTML) - 1;    
+         minutesOnes.innerHTML = parseInt(minutesOnes.innerHTML) + 10;
+         console.log("less than 20 minutes");
+        }
+
        if (parseInt(secondsOnes.innerHTML) === 0 && parseInt(secondsTens.innerHTML) === 0) {
         secondsTens.innerHTML = parseInt(secondsTens.innerHTML) + 5;
         secondsOnes.innerHTML = parseInt(secondsOnes.innerHTML) + 9;
@@ -36,13 +43,10 @@ function timeDown() {
 
        if (parseInt(secondsOnes.innerHTML) === 0) {
         secondsTens.innerHTML = parseInt(secondsTens.innerHTML) - 1;
-        secondsOnes.innerHTML = parseInt(secondsOnes.innerHTML) + 10;
+        secondsOnes.innerHTML = parseInt(secondsOnes.innerHTML) + 9;
         console.log('tenths of a second have lowered');
        }
 
-       if (parseInt(secondsOnes.innerHTML) === 0 && parseInt(secondsTens.innerHTML) === 0 && parseInt(minutesOnes.innerHTML) === 0) {
-        minutesTens.innerHTML = parseInt(minutesTens.innerHTML) - 1;    
-       }
 
        else {
         secondsOnes.innerHTML = parseInt(secondsOnes.innerHTML) - 1;
